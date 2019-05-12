859409d9f2f4f2b55cc4d0278aea50b1f9263128
summerchen <610918446@qq.com>
2019-05-12 16:14:00 +0800

    轮播

---
+++ main.js
@@ -0,0 +1,42 @@
+let n
+initial()
+setInterval(()=>{
+    makeLeave(getImage(n))
+    .one('transitionend', function(e){
+        makeEnter($(e.currentTarget))
+    })
+    makeCurrent(getImage(n+1))
+    n +=1
+},3000)
+
+
+
+
+
+
+function x(n){
+    if(n>3){
+        n = n%3
+        if(n===0){
+            n = 3
+        }
+    }
+    return n
+}
+function getImage(n){
+   return $(`.images > img:nth-child(${x(n)})`) 
+}
+function initial(){
+    n = 1
+    $(`.images > img:nth-child(${n})`).addClass('current')
+    .siblings().addClass('enter')
+}
+function makeCurrent($node){
+    return $node.removeClass('enter leave').addClass('current')
+}
+function makeEnter($node){
+    return $node.removeClass('current leave').addClass('enter')
+}
+function makeLeave($node){
+    return $node.removeClass('current enter').addClass('leave')
+}
